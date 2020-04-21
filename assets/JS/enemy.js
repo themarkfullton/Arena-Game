a5366e121fe476d273a4159378d72298a6bdcb0e
Mark F <the.mark.fullton@gmail.com>
2020-04-21 14:16:32 -0700

    first commit

---
+++ assets/JS/enemy.js
@@ -0,0 +1,11 @@
+function Enemy(enemyType, health, constitution, strength, defense, agility, dexterity, intellect, endurance) {
+    this.enemyType = enemyType;
+    this.health = health;
+    this.constitution = constitution;
+    this.strength = strength;
+    this.defense = defense;
+    this.agility = agility;
+    this.dexterity = dexterity;
+    this.intellect = intellect;
+    this.endurance = endurance;
+}
---
+++ assets/JS/player.js
@@ -0,0 +1,18 @@
+let player;
+
+function Player(classType, level, health, constitution, exp, gold, strength, defense, agility, dexterity, intellect, endurance) {
+    this.classType = classType;
+    this.level = level;
+    this.health = health;
+    this.constitution = constitution;
+    this.exp = exp;
+    this.gold = gold;
+    this.itemL = ["Potion"];
+    this.strength = strength;
+    this.defense = defense;
+    this.agility = agility;
+    this.dexterity = dexterity;
+    this.intellect = intellect;
+    this.endurance = endurance;
+}
+
