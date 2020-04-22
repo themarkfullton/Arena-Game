bbfde6eeebde08014a5bb173b093ddd9a1edab76
Mark F <the.mark.fullton@gmail.com>
2020-04-22 12:40:00 -0700

    Added and styled health, constitution, and Experience bars

--- assets/JS/controller.js
+++ assets/JS/controller.js
@@ -7,24 +7,24 @@ let controller = {
     resetPlayer: function(classType){
         switch (classType) {
             case "Adventurer":
-                player = new Player (classType, 100, 0, 30, 30, 10, 15, 0, 10 );
+                player = new Player (classType, 100, 5, 30, 30, 10, 15, 0, 10, "Guns-a-Blazing" );
                 break;
             case "Outlaw":
-                player = new Player (classType, 85, 0, 10, 15, 30, 30, 0, 10 );
+                player = new Player (classType, 85, 5, 10, 15, 30, 30, 0, 10, "Shadow Slash" );
                 break;
             case "Doctor":
-                player = new Player (classType, 70, 0, 15, 15, 20, 15, 15, 10 );
+                player = new Player (classType, 70, 10, 15, 15, 20, 15, 15, 10, "First Aid" );
                 break;
             case "Cultist":
-                player = new Player (classType, 70, 90, 0, 10, 15, 20, 15, 20 );
+                player = new Player (classType, 70, 90, 0, 10, 15, 20, 15, 20, "Fireball");
                 break;
             case "Madman":
-                player = new Player (classType, 60, 100, 0, 10, 15, 20, 25, 15 );
+                player = new Player (classType, 60, 100, 0, 10, 15, 20, 25, 15, "" );
                 break;
         }
 
         let getInterface = document.querySelector(".interface");
-        getInterface.innerHTML = '';
+        getInterface.innerHTML = '<img src="assets/images/icon-player/icon-' + classType.toLowerCase() + '.png" alt="' + classType + '" class="batIconPic"><div><h3>' + classType +'</h3><progress id="healthBar" value="' + player.health + '" max="' + player.health + '"></progress>   <progress id="constBar" value="' + player.constitution + '" max="' + player.constitution + '"></progress><p>Experience: ' + player.exp + '</p><p>Gold: ' + player.gold + '</p><p>Strength: ' + player.strength + '</p><p>Defense: ' + player.defense + '</p><p>Agility: ' + player.agility + '</p><p>Dexterity: ' + player.dexterity + '</p><p>Intellect: ' + player.intellect + '</p><p>Endurance: ' + player.endurance + '</p></div>';
     },
 
     setPreFight: function(){
--- assets/JS/player.js
+++ assets/JS/player.js
@@ -1,18 +1,20 @@
 let player;
 
-function Player(classType, health, constitution, strength, defense, agility, dexterity, intellect, endurance) {
+function Player(classType, health, constitution, strength, defense, agility, dexterity, intellect, endurance, startAbility) {
     this.classType = classType;
     this.level = 1;
     this.health = health;
     this.constitution = constitution;
     this.exp = 0;
     this.gold = 0;
-    this.itemL = ["Potion"];
     this.strength = strength;
     this.defense = defense;
     this.agility = agility;
     this.dexterity = dexterity;
     this.intellect = intellect;
     this.endurance = endurance;
+    this.toNextLvl = 100;
+    this.items = ["potion"];
+    this.abilities = [startAbility];
 }
 
