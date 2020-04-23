39a5239bf2431f6729824d84cc763549bfda5765
Mark F <the.mark.fullton@gmail.com>
2020-04-22 19:53:47 -0700

    Added attack function

--- assets/JS/enemy.js
+++ assets/JS/enemy.js
@@ -1,17 +1,20 @@
 let enemy;
 
-function Enemy(enemyType, health, constitution, strength, defense, agility, dexterity, intellect, endurance, abilities, description) {
+function Enemy(enemyType, enemyFS, exp, health, constitution, strength, defense, speed, dexterity, intellect, endurance, reward, abilities, description) {
     this.enemyType = enemyType;
+    this.enemyFS = enemyFS;
+    this.exp = exp;
     this.health = health;
     this.curHealth = health;
     this.constitution = constitution;
     this.curConstitution = constitution;
     this.strength = strength;
     this.defense = defense;
-    this.agility = agility;
+    this.speed = speed;
     this.dexterity = dexterity;
     this.intellect = intellect;
     this.endurance = endurance;
+    this.reward = reward;
     this.abilities = abilities;
     this.description = description;
 }
