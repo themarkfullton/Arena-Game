0587cfe59e9c224ad4aaaaf143d22ac7a2f8508a
Mark F <the.mark.fullton@gmail.com>
2020-04-30 14:18:39 -0700

    Added setFight function innerHTML

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -43,37 +43,7 @@ let controller = {
     setFight: function(){
 
 
-        $("#interface").html('<div id="battlescreen"><div class="row d-flex justify-content-between" id="insideBattle"><div id="battlePlayer"><div id="battlePlayerName"><h3>' + player.pName + '<span id="battleLvl">Level' + player.level + '</span></h3></div><div id="battlePlayerBars" class="col-lg-4"><div class ="progress" id="playerHealthBar" data-label="Health"><span class="healthBar" style="width: ' + ((player.curHealth/player.health)*100) +'%"></span></div><div class ="progress" id="playerWillBar" data-label="Willpower"><span class="constBar" style="width: ' + ((player.curWill/player.willpower)*100) + '%"></span></div><div class ="progress" id="playerExpBar" data-label="Experience"><span class="expBar" style="width: ' + ((player.exp/player.toNextLvl)*100) +'"></span></div></div></div><div id="battleEnemy"><div id="battleEnemyName">
-                    <h3>Ghast <span id="battleLvl">Level1</span></h3>
-                </div>
-                <div id="battleEnemyBars" class="col-lg-4">
-                    <div class ="progress" id="enemyHealthBar" data-label="Health"><span class="healthBar" style="width: 50%"></span></div>
-                    <div class ="progress" id="enemyWillBar" data-label="Willpower"><span class="constBar" style="width: 50%"></span></div>
-                </div>
-            </div>
-        </div>
-        <div class="row d-flex justify-content-between" id="insideBattlePicRow">
-            <div id="battlePlayerPic">
-                <img class="battlePic" src="assets/images/Icons/Players/cultist00.png">
-            </div>
-            <div id="battleEnemyPic">
-                <img class="battlePic" src="assets/images/Icons/Enemies/ghast.png">
-            </div>
-        </div>
-    </div>
-
-    <div id="battleNotes">
-        <p class="bNPlayer">You attack 5 times for 10 damage</p>
-        <p class="bNEnemy">Ghast attacks 3 times for 29 damage</p>
-        <p>You lose</p>
-    </div>
-
-    <div id="actions">
-        <div class="row justify-content-center">
-            <div class="ability" onclick="">Fireball</div>
-            <div class="ability" onclick="">Possession</div>
-        </div>
-    </div>--></div>');
+        $("#interface").html('<div id="battlescreen"><div class="row d-flex justify-content-between" id="insideBattle"><div id="battlePlayer"><div id="battlePlayerName"><h3>' + player.pName + '<span id="battleLvl">Level' + player.level + '</span></h3></div><div id="battlePlayerBars" class="col-lg-4"><div class ="progress" id="playerHealthBar" data-label="Health"><span class="healthBar" style="width: ' + ((player.curHealth/player.health)*100) +'%"></span></div><div class ="progress" id="playerWillBar" data-label="Willpower"><span class="constBar" style="width: ' + ((player.curWill/player.willpower)*100) + '%"></span></div><div class ="progress" id="playerExpBar" data-label="Experience"><span class="expBar" style="width: ' + ((player.exp/player.toNextLvl)*100) +'"></span></div></div></div><div id="battleEnemy"><div id="battleEnemyName"><h3>' + enemy.disClassType + '<span id="battleLvl">Level' + enemy.level + '</span></h3></div><div id="battleEnemyBars" class="col-lg-4"><div class ="progress" id="enemyHealthBar" data-label="Health"><span class="healthBar" style="width: ' + ((enemy.curHealth/enemy.health)*100) + '%"></span></div><div class ="progress" id="enemyWillBar" data-label="Willpower"><span class="constBar" style="width: ' + ((enemy.curWill/enemy.will)*100) + '%"></span></div></div></div></div><div class="row d-flex justify-content-between" id="insideBattlePicRow"><div id="battlePlayerPic"><img class="battlePic" src="assets/images/Icons/Players/cultist00.png"></div><div id="battleEnemyPic"><img class="battlePic" src="assets/images/Icons/Enemies/ghast.png"></div></div></div><div id="battleNotes"><p>A ' + enemy.disClassType + ' appears</p></div><div id="actions"><div class="row justify-content-center"></div></div></div>');
     },
 
     
