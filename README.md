b17af53e3ce6ec632511ef2e1049448911ad6ab1
Mark F <the.mark.fullton@gmail.com>
2020-06-05 10:13:42 -0700

    Added Link to live to ReadMe and used Prettier on controller.js to make it a little easier to read

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Dreamlands - A Lovecraftian Arena game
-The first thing you need to know is that this game isn't quite complete--it is in the process of being worked on. However, it IS in a functioning state... just not where I want it to be.
+
+**Link to Live:** https://themarkfullton.github.io/Arena-Game/
 
 This is a spin on the classic arena style game. You create a character and go toe-to-toe with enemies that are randomized depending on your level.
 
@@ -8,9 +9,11 @@ There's supposed to be a whole story arc, bosses, abilities AND an inventory sys
 Also, please note, the images here are placeholders. I don't own any of them and will be replacing them with my own things. Owing to time constraints, however, I chose to focus on the game function.
 
 ## Build Status
+
 In progress.
 
 Needs added:
+
 - Ability system
 - Inventory system
 - Bosses
@@ -18,7 +21,9 @@ Needs added:
 - Original images
 
 ## Languages Used
+
 HTML, CSS, JQuery, Javascript
 
 ## Motivation
-Completed as an assignment for UCLA Coding Bootcamp. Mixed things up quite a bit to entertain myself. Chose Lovecraft because it was easiest to create a list of enemies to fight.
+
+Created during the coronavirus quarantine as a way to amuse my friends (big Lovecraft buffs).
--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,194 +1,271 @@
 /* Essentially runs the game */
 
 let controller = {
-    // Creates player and sends to Confirm Character Screen
-    setGameStart: function(pId, classType){
-        this.createPlayer(pId, classType);
-        script.charConfScreen();
-    },
+  // Creates player and sends to Confirm Character Screen
+  setGameStart: function (pId, classType) {
+    this.createPlayer(pId, classType);
+    script.charConfScreen();
+  },
 
-    // Creates a player
-    createPlayer: function(pId, classType){
-        player = new Player (pId, classType);
-    },
+  // Creates a player
+  createPlayer: function (pId, classType) {
+    player = new Player(pId, classType);
+  },
 
-    // Names the created player
-    namePlayer: function(){
-        player.pName = $('#pName').val();
-        script.scriptIntro();
-    },
+  // Names the created player
+  namePlayer: function () {
+    player.pName = $("#pName").val();
+    script.scriptIntro();
+  },
 
-    // Depending on the level, different enemies are availiable.
-    // This function creates a random number based on player level.
-    setRanNum: function(){
-        let sRanNum;
+  // Depending on the level, different enemies are availiable.
+  // This function creates a random number based on player level.
+  setRanNum: function () {
+    let sRanNum;
 
-        if (player.level == 1){
-            sRanNum = Math.floor(Math.random() * Math.floor(4));
+    if (player.level == 1) {
+      sRanNum = Math.floor(Math.random() * Math.floor(4));
+    } else if (player.level == 2) {
+      sRanNum = Math.floor(Math.random() * Math.floor(8));
+    } else if (player.level == 3) {
+      sRanNum = Math.floor(Math.random() * Math.floor(13));
+    } else if (player.level >= 4) {
+      sRanNum = Math.floor(Math.random() * Math.floor(bestiary.length));
+    }
+
+    return sRanNum;
+  },
+
+  // Creates an enemy with the random number generated
+  setEnemy: function () {
+    enemy = new Enemy(this.setRanNum());
+  },
+
+  // Sets up the battle screen with a new enemy
+  setFight: function () {
+    player.curHealth = player.health;
+    this.setEnemy();
+
+    $("#interface").html(
+      '<div id="battlescreen"><div class="row d-flex justify-content-between" id="insideBattle"><div id="battlePlayer"><div id="battlePlayerName"><h3>' +
+        player.pName +
+        '<span id="battleLvl">Level' +
+        player.level +
+        '</span></h3></div><div id="battlePlayerBars" class="col-lg-4"><div class ="progress" id="playerHealthBar" data-label="Health"><span class="healthBar" style="width: ' +
+        (player.curHealth / player.health) * 100 +
+        '%"></span></div><div class ="progress" id="playerWillBar" data-label="Willpower"><span class="constBar" style="width: ' +
+        (player.curWill / player.willpower) * 100 +
+        '%"></span></div><div class ="progress" id="playerExpBar" data-label="Experience"><span class="expBar" style="width: ' +
+        (player.exp / player.toNextLvl) * 100 +
+        '%"></span></div></div></div><div id="battleEnemy"><div id="battleEnemyName"><h3>' +
+        enemy.disClassType +
+        '<span id="battleLvl">Level' +
+        enemy.level +
+        '</span></h3></div><div id="battleEnemyBars" class="col-lg-4"><div class ="progress" id="enemyHealthBar" data-label="Health"><span class="healthBar" style="width: ' +
+        (enemy.curHealth / enemy.health) * 100 +
+        '%"></span></div><div class ="progress" id="enemyWillBar" data-label="Willpower"><span class="constBar" style="width: ' +
+        (enemy.curWill / enemy.willpower) * 100 +
+        '%"></span></div></div></div></div><div class="row d-flex justify-content-between" id="insideBattlePicRow"><div id="battlePlayerPic"><img class="battlePic" src="assets/images/Icons/Players/' +
+        player.classType +
+        player.pId +
+        '.png"></div><div id="battleEnemyPic"><img class="battlePic" src="assets/images/Icons/Enemies/' +
+        enemy.classType +
+        '.png"></div></div></div><div id="battleNotes"><p>A ' +
+        enemy.disClassType +
+        ' appears</p></div><div id="actionDiv"><div id="actions" class="row justify-content-center"> <div class="ability" onclick="controller.battleRound(0)">Attack</div></div></div></div><div id="treasureBox" class="d-flex justify-content-center"><h5>Gold: <span class="treasureAmt">' +
+        player.gold +
+        "</span></h5></div>"
+    );
+
+    $("#battlescreen").css(
+      "background-image",
+      "url('assets/images/setting/" + player.level.toString() + ".png"
+    );
+  },
+
+  //Runs the battle
+  battleRound: function (selAction) {
+    let check = 0;
+    if (player.curHealth > 0 && enemy.curHealth > 0) {
+      if (player.agility >= enemy.agility) {
+        console.log("P Speed " + player.agility);
+        console.log("E Speed " + enemy.agility);
+        console.log("Player Faster");
+        this.playerTurn(selAction);
+        check = this.checkWin();
+        if (check == 0) {
+          this.enemyTurn();
+          check = this.checkWin();
         }
-        else if (player.level == 2){
-            sRanNum = Math.floor(Math.random() * Math.floor(8));
+      } else {
+        console.log("P Speed " + player.agility);
+        console.log("E Speed " + enemy.agility);
+        console.log("Enemy Faster");
+        this.enemyTurn();
+        check = this.checkWin();
+        if (check == 0) {
+          this.playerTurn(selAction);
+          check = this.checkWin();
         }
-        else if (player.level == 3){
-            sRanNum = Math.floor(Math.random() * Math.floor(13));
-        }
-        else if (player.level >= 4){
-            sRanNum = Math.floor(Math.random() * Math.floor(bestiary.length));
-        }
-        
-            return sRanNum;
-    },
+      }
+    }
+  },
 
-    // Creates an enemy with the random number generated
-    setEnemy: function(){
-        enemy = new Enemy(this.setRanNum());
-    },
+  //Runs the player turn; you can see that I've set it up for the player to select other moves... but only have one move. Time constraints.
+  playerTurn(selAction) {
+    let baseDamage = PlayerMoves[selAction]();
 
-    // Sets up the battle screen with a new enemy
-    setFight: function(){
-        player.curHealth = player.health;
-        this.setEnemy();
+    enemy.curHealth = enemy.curHealth - baseDamage;
 
-        $("#interface").html('<div id="battlescreen"><div class="row d-flex justify-content-between" id="insideBattle"><div id="battlePlayer"><div id="battlePlayerName"><h3>' + player.pName + '<span id="battleLvl">Level' + player.level + '</span></h3></div><div id="battlePlayerBars" class="col-lg-4"><div class ="progress" id="playerHealthBar" data-label="Health"><span class="healthBar" style="width: ' + ((player.curHealth/player.health)*100) +'%"></span></div><div class ="progress" id="playerWillBar" data-label="Willpower"><span class="constBar" style="width: ' + ((player.curWill/player.willpower)*100) + '%"></span></div><div class ="progress" id="playerExpBar" data-label="Experience"><span class="expBar" style="width: ' + ((player.exp/player.toNextLvl)*100) +'%"></span></div></div></div><div id="battleEnemy"><div id="battleEnemyName"><h3>' + enemy.disClassType + '<span id="battleLvl">Level' + enemy.level + '</span></h3></div><div id="battleEnemyBars" class="col-lg-4"><div class ="progress" id="enemyHealthBar" data-label="Health"><span class="healthBar" style="width: ' + ((enemy.curHealth/enemy.health)*100) + '%"></span></div><div class ="progress" id="enemyWillBar" data-label="Willpower"><span class="constBar" style="width: ' + ((enemy.curWill/enemy.willpower)*100) + '%"></span></div></div></div></div><div class="row d-flex justify-content-between" id="insideBattlePicRow"><div id="battlePlayerPic"><img class="battlePic" src="assets/images/Icons/Players/'+ player.classType + player.pId +'.png"></div><div id="battleEnemyPic"><img class="battlePic" src="assets/images/Icons/Enemies/' + enemy.classType + '.png"></div></div></div><div id="battleNotes"><p>A ' + enemy.disClassType + ' appears</p></div><div id="actionDiv"><div id="actions" class="row justify-content-center"> <div class="ability" onclick="controller.battleRound(0)">Attack</div></div></div></div><div id="treasureBox" class="d-flex justify-content-center"><h5>Gold: <span class="treasureAmt">' + player.gold + '</span></h5></div>');
-    
-        $("#battlescreen").css("background-image", "url('assets/images/setting/"+ player.level.toString() +".png");
-    },
+    if (enemy.curHealth < 0) {
+      enemy.curHealth = 0;
+    }
 
-    //Runs the battle
-    battleRound: function(selAction){
-        let check = 0;
-        if (player.curHealth > 0 && enemy.curHealth >0){
-                        
-            if (player.agility >= enemy.agility){
-                console.log("P Speed " + player.agility);
-                console.log("E Speed " + enemy.agility);
-                console.log("Player Faster");
-                this.playerTurn(selAction);
-                check = this.checkWin();
-                if (check == 0){
-                    this.enemyTurn();
-                    check = this.checkWin();
-                }
-            }
-            else {
-                console.log("P Speed " + player.agility);
-                console.log("E Speed " + enemy.agility);
-                console.log("Enemy Faster");
-                this.enemyTurn();
-                check = this.checkWin();
-                if (check == 0){
-                    this.playerTurn(selAction);
-                    check = this.checkWin();
-                }
-            }
-        }
-    },
+    $("#playerHealthBar").html(
+      '<span class="healthBar" style="width: ' +
+        (player.curHealth / player.health) * 100 +
+        '%"></span>'
+    );
+    $("#playerWillBar").html(
+      '<span class="constBar" style="width: ' +
+        (player.curWill / player.willpower) * 100 +
+        '%"></span>'
+    );
+    $("#enemyHealthBar").html(
+      '<span class="healthBar" style="width: ' +
+        (enemy.curHealth / enemy.health) * 100 +
+        '%"></span>'
+    );
+    $("#enemyWillBar").html(
+      '<span class="constBar" style="width: ' +
+        (enemy.curWill / enemy.willpower) * 100 +
+        '%"></span>'
+    );
+  },
 
-    //Runs the player turn; you can see that I've set it up for the player to select other moves... but only have one move. Time constraints.
-    playerTurn(selAction){
-        let baseDamage = PlayerMoves[selAction]();
+  //Runs the enemy turn
+  enemyTurn() {
+    let baseDamage = PlayerMoves[29]();
 
-        enemy.curHealth = enemy.curHealth - baseDamage;
+    player.curHealth = player.curHealth - baseDamage;
 
-        if (enemy.curHealth < 0 ){
-            enemy.curHealth = 0;
-        }
+    if (player.curHealth < 0) {
+      player.curHealth = 0;
+    }
 
-        $("#playerHealthBar").html('<span class="healthBar" style="width: ' + ((player.curHealth/player.health)*100) +'%"></span>');
-        $("#playerWillBar").html('<span class="constBar" style="width: ' + ((player.curWill/player.willpower)*100) + '%"></span>');
-        $("#enemyHealthBar").html('<span class="healthBar" style="width: ' + ((enemy.curHealth/enemy.health)*100) + '%"></span>');
-        $("#enemyWillBar").html('<span class="constBar" style="width: ' + ((enemy.curWill/enemy.willpower)*100) + '%"></span>');
-        
-    },
+    $("#playerHealthBar").html(
+      '<span class="healthBar" style="width: ' +
+        (player.curHealth / player.health) * 100 +
+        '%"></span>'
+    );
+    $("#playerWillBar").html(
+      '<span class="constBar" style="width: ' +
+        (player.curWill / player.willpower) * 100 +
+        '%"></span>'
+    );
+    $("#enemyHealthBar").html(
+      '<span class="healthBar" style="width: ' +
+        (enemy.curHealth / enemy.health) * 100 +
+        '%"></span>'
+    );
+    $("#enemyWillBar").html(
+      '<span class="constBar" style="width: ' +
+        (enemy.curWill / enemy.willpower) * 100 +
+        '%"></span>'
+    );
+  },
 
-    //Runs the enemy turn
-    enemyTurn(){
-        let baseDamage = PlayerMoves[29]();
+  // Checks if the player has won or lost the round; if so, it adjusts accordingly
+  checkWin() {
+    if (enemy.curHealth <= 0) {
+      var newDiv = $("<div>");
 
-        player.curHealth = player.curHealth - baseDamage;
+      newDiv.html(
+        '<p class="bNEnemy">' +
+          enemy.disClassType +
+          " crumples in defeat.</p><p>You win " +
+          enemy.exp +
+          " experience and " +
+          enemy.gold +
+          " gold!</p>"
+      );
 
-        if (player.curHealth < 0 ){
-            player.curHealth = 0;
-        }
+      $("#battleNotes").prepend(newDiv);
 
-        $("#playerHealthBar").html('<span class="healthBar" style="width: ' + ((player.curHealth/player.health)*100) +'%"></span>');
-        $("#playerWillBar").html('<span class="constBar" style="width: ' + ((player.curWill/player.willpower)*100) + '%"></span>');
-        $("#enemyHealthBar").html('<span class="healthBar" style="width: ' + ((enemy.curHealth/enemy.health)*100) + '%"></span>');
-        $("#enemyWillBar").html('<span class="constBar" style="width: ' + ((enemy.curWill/enemy.willpower)*100) + '%"></span>');
-    },
+      player.exp = player.exp + enemy.exp;
+      console.log("P Exp" + player.exp);
 
-    // Checks if the player has won or lost the round; if so, it adjusts accordingly
-    checkWin(){
-        if (enemy.curHealth <= 0) {
-            var newDiv = $("<div>");
+      player.gold = player.gold + enemy.gold;
 
-            newDiv.html('<p class="bNEnemy">' + enemy.disClassType + ' crumples in defeat.</p><p>You win ' + enemy.exp + ' experience and ' + enemy.gold + ' gold!</p>');
+      if (player.exp >= player.toNextLvl) {
+        controller.levelUp();
 
-            $("#battleNotes").prepend(newDiv);
+        var newDiv = $("<div>");
 
-            player.exp = player.exp + enemy.exp;
-            console.log("P Exp" + player.exp);
+        newDiv.html(
+          '<p class="bNPlayer">LEVEL UP!!!</p><p>You reached Level ' +
+            player.level +
+            "!</p>"
+        );
 
-            player.gold = player.gold + enemy.gold;
+        $("#battleNotes").prepend(newDiv);
 
-            if(player.exp >= player.toNextLvl){                
-                controller.levelUp();
+        $("#battleLvl").html("Level" + player.level);
+      }
+      $("#playerExpBar").html(
+        '<span class="expBar" style="width: ' +
+          (player.exp / player.toNextLvl) * 100 +
+          '%"></span>'
+      );
+      $("#treasureBox").html(
+        '<h5>Gold: <span class="treasureAmt">' + player.gold + "</span></h5>"
+      );
 
-                var newDiv = $("<div>");
+      var newDiv = $("<div>");
 
-                newDiv.html('<p class="bNPlayer">LEVEL UP!!!</p><p>You reached Level ' + player.level + '!</p>');
-                
-                $("#battleNotes").prepend(newDiv);
-                
-                $("#battleLvl").html("Level" + player.level);
-                
-            }
-            $("#playerExpBar").html('<span class="expBar" style="width: ' + ((player.exp/player.toNextLvl)*100) +'%"></span>');
-            $("#treasureBox").html('<h5>Gold: <span class="treasureAmt">' + player.gold + '</span></h5>');
+      newDiv.html(
+        '<p>Unfortunately, this is still in progress, but you can continue to play, level up and gather gold.</p><button type="button" class="btn btn-primary btn-lg btn-block" onclick="controller.setFight()">Click Here to Fight New Enemy!</button>'
+      );
 
-            var newDiv = $("<div>");
+      $("#battleNotes").prepend(newDiv);
 
-            newDiv.html('<p>Unfortunately, this is still in progress, but you can continue to play, level up and gather gold.</p><button type="button" class="btn btn-primary btn-lg btn-block" onclick="controller.setFight()">Click Here to Fight New Enemy!</button>');
-                
-            $("#battleNotes").prepend(newDiv);
+      return 1;
+    } else if (player.curHealth <= 0) {
+      var newDiv = $("<div>");
 
-            return 1;
-        }
-        else if (player.curHealth <= 0) {
-            var newDiv = $("<div>");
+      newDiv.html(
+        '<p class="nNPlayer">You crumple over in defeat.</p><p>You lose!</p><button type="button" class="btn btn-primary btn-lg btn-block" onclick="script.charSelectScreen()">Click Here to Restart!</button>'
+      );
 
-            newDiv.html('<p class="nNPlayer">You crumple over in defeat.</p><p>You lose!</p><button type="button" class="btn btn-primary btn-lg btn-block" onclick="script.charSelectScreen()">Click Here to Restart!</button>');
+      $("#battleNotes").prepend(newDiv);
 
-            $("#battleNotes").prepend(newDiv);
+      return 2;
+    }
 
-            return 2;
-        }
-        
-        return 0;
-    },
+    return 0;
+  },
 
-    //Increases player level and ups their stats
-    levelUp: function() {
-        player.level++;
-        player.toNextLvl = player.toNextLvl+ 30;
-        
-        if (player.classType == "heir" || player.classType == "outlaw"){
-            player.health = player.health + 10;
-            player.willpower = player.willpower + 3;
-            player.strength = player.strength + 5;
-            player.defense = player.defense + 4;
-            player.dexterity = player.dexterity + 3;
-            player.tenacity = player.tenacity+ 2;
-        }
-        else{
-            player.health = player.health + 6;
-            player.willpower = player.willpower + 6;
-            player.intelligence = player.intelligence + 5;
-            player.defense = player.defense + 3;
-            player.dexterity = player.dexterity + 3;
-            player.tenacity = player.tenacity + 5;
-        }
+  //Increases player level and ups their stats
+  levelUp: function () {
+    player.level++;
+    player.toNextLvl = player.toNextLvl + 30;
 
-        player.exp = 0;
-    },
-}
+    if (player.classType == "heir" || player.classType == "outlaw") {
+      player.health = player.health + 10;
+      player.willpower = player.willpower + 3;
+      player.strength = player.strength + 5;
+      player.defense = player.defense + 4;
+      player.dexterity = player.dexterity + 3;
+      player.tenacity = player.tenacity + 2;
+    } else {
+      player.health = player.health + 6;
+      player.willpower = player.willpower + 6;
+      player.intelligence = player.intelligence + 5;
+      player.defense = player.defense + 3;
+      player.dexterity = player.dexterity + 3;
+      player.tenacity = player.tenacity + 5;
+    }
+
+    player.exp = 0;
+  },
+};
